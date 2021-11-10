20205f490cbcb09d369fb95c18e889fff884c498
besterz274448@gmail.com <besterz274448@gmail.com>
2021-11-11 01:11:10 +0700

    add artistcard component

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import "../asset/components.css";
 
-function ArtistCard() {
-    return (
-        <div>
-            ArtistCard Hello
+function ArtistCard({
+  src = "",
+  name = "Edward Snowden",
+  contract = "Snowden",
+  artistDescription = "I used to work for the government. Now I work for the public. President, Freedom of the Press Foundation",
+}) {
+  return (
+    <div className="image-card-container">
+      <div className="artist-card-container">
+        <img className="artist-card-img" src={src} alt="imgCard"></img>
+      </div>
+      <div className="artist-detail-container">
+        <div className="artist-detail-avatar-box">
+            <div><img src={src} width="70px" height="70px"/></div>
         </div>
-    )
+        <div className="artist-detail-name b">{name}</div>
+        <div className="artist-detail-contract b">
+          <span>@</span>
+          <span>{contract}</span>
+        </div>
+        <div className="artist-detail-description">{artistDescription.length > 120 ? artistDescription.substring(0,120) + "..." : artistDescription }</div>
+      </div>
+    </div>
+  );
 }
 
-export default ArtistCard
+export default ArtistCard;
--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../asset/main.css";
 import Grid from "@mui/material/Grid";
 import Accord from "../components/Accord";
-import PriceRange from "../components/PriceRange";
+import ArtistCard from "../components/ArtistCard";
 import SelectBox from "../components/SelectBox";
 
 function Profiles() {
@@ -17,7 +17,19 @@ function Profiles() {
           <SelectBox option={sc_options} />
         </Accord>
       </div>
-      <div className="feed-items"></div>
+      <div className="feed-items">
+        <Grid alignItems="center" justifyContent="center" rowGap={3} container spacing={2}>
+          <Grid item lg={4} md={6} sm={6} xs={11}>
+            <ArtistCard src={"https://picsum.photos/200/300?random=" + Math.floor(Math.random() * 1000)}/>
+          </Grid>
+          <Grid item lg={4} md={6} sm={6} xs={11}>
+            <ArtistCard src={"https://picsum.photos/200/300?random=" + Math.floor(Math.random() * 1000)}/>
+          </Grid>
+          <Grid item lg={4} md={6} sm={6} xs={11}>
+            <ArtistCard src={"https://picsum.photos/200/300?random=" + Math.floor(Math.random() * 1000)}/>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
