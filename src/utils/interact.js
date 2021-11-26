ec775955d115257646bf42c05b903d83716968e5
Besterz274448 <besterz274448@gmail.com>
2021-11-26 14:46:24 +0700

    minting nft process

---
+++ src/utils/interact.js
@@ -0,0 +1,36 @@
+//ฟังก์ชันสำหรับตรวจสอบว่ามี metamask อยู่ใน browser รึเปล่าและต่อกับ metamask 
+export const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const obj = {
+          status: "👆🏽 Write a message in the text-field above.",
+          address: addressArray[0],
+        };
+        return obj;
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
